8be78f2a2658a4f3fafcd6c6c9b61c81c30a1005
Antony Jones <aj@desirableobjects.co.uk>
2017-09-25 15:16:50 +0100

    initial app

---
+++ main.js
@@ -0,0 +1,32 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  const dashboard = process.argv[2] || 'http://demo.vudash.com/default.dashboard'
+  mainWindow = new BrowserWindow({ frame: false })
+  mainWindow.loadURL(dashboard)
+  mainWindow.setFullScreen(true)
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
